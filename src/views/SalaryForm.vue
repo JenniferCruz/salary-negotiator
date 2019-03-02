3e0714bbeab9448318a21029be6c4da2f99d157b
Jennifer Cruz <JenniferCruz@github.com>
2019-03-02 15:28:34 +0100

    feat: Forms report error if input is not a positive number

--- src/views/SalaryForm.vue
+++ src/views/SalaryForm.vue
@@ -1,27 +1,61 @@
 <template>
     <div v-bind:id="formId">
-        <form></form>
+        <form @submit.prevent="onSubmit">
+
+            <div v-if="invalidInput" id="error-messages" class="warning-message">
+                <p>{{ errorMessage }}</p>>
+            </div>
+
+            <input v-bind:id="name" v-model="amount" v-bind:placeholder="placeholder">
+            <input type="submit" value="Submit">
+
+        </form>
     </div>
 </template>
 
 <script>
     export default {
         name: "SalaryForm",
+        data() {
+            return {
+                amount: null,
+                // invalidInput: false,
+            }
+        },
         props: {
             name: {
                 type: String,
                 required: true
+            },
+            placeholder: {
+                type: String,
+                default: "Enter amount"
+            }
+        },
+        methods: {
+            onSubmit() {
+                // TODO
             }
         },
         computed: {
             formId() {
-                return `${this.name}-form`
-                // return "eureka";
+                return `${this.name.toLowerCase()}-form`
+            },
+            errorMessage() {
+                return "Please, enter a numeric value"
+            },
+            invalidInput() {
+                const n = (new Number(this.amount)).valueOf();
+                return Number.isNaN(n) || n < 0;
             }
         }
     }
 </script>
 
 <style scoped>
-
+    .warning-message {
+        background-color: lightpink;
+        color: deeppink;
+        border: 1px solid hotpink;
+    }
 </style>
--- tests/unit/negotiatorApp.spec.js
+++ tests/unit/negotiatorApp.spec.js
@@ -1,6 +1,7 @@
-import { shallowMount, mount } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
 import NegotiatorApp from "@/NegotiatorApp.vue";
 import router from "@/router";
+import SalaryForm from "../../src/views/SalaryForm";
 
 describe("App contains two tabs", () => {
   it("renders tabs for employer and employee", () => {
@@ -13,4 +14,83 @@ describe("App contains two tabs", () => {
     const expectedTabs = ["employer", "employee"];
     expect(tabs).toEqual(expect.arrayContaining(expectedTabs));
   });
+});
+
+describe("Salary Form takes only numbers", () => {
+
+  it("Rejects strings", () => {
+
+    const formWrapper = shallowMount(SalaryForm, {
+      propsData: {
+        name: "employee"
+      }
+    });
+
+    const input = formWrapper.find("input");
+    input.element.value = 'input';
+    input.trigger('input');
+    formWrapper.find('input[type=submit]').trigger('click');
+
+    const error = formWrapper.find("#error-messages");
+
+    expect(error.exists()).toBeTruthy();
+
+  });
+
+
+
+  it("Rejects negative numbers", () => {
+
+    const formWrapper = shallowMount(SalaryForm, {
+      propsData: {
+        name: "employee"
+      }
+    });
+
+    const input = formWrapper.find("input");
+    input.element.value = -1;
+    input.trigger('input');
+    formWrapper.find('input[type=submit]').trigger('click');
+
+    const error = formWrapper.find("#error-messages");
+
+    expect(error.exists()).toBeTruthy();
+
+  });
+
+
+  it("Accepts integer numbers", () => {
+    const formWrapper = shallowMount(SalaryForm, {
+      propsData: {
+        name: "employee"
+      }
+    });
+
+    const input = formWrapper.find("input");
+    input.element.value = 1;
+    input.trigger('input');
+    formWrapper.find('input[type=submit]').trigger('click');
+
+    const error = formWrapper.find("#error-messages");
+
+    expect(error.exists()).toBeFalsy();
+  });
+
+  it("Accepts decimal numbers", () => {
+    const formWrapper = shallowMount(SalaryForm, {
+      propsData: {
+        name: "employee"
+      }
+    });
+
+    const input = formWrapper.find("input");
+    input.element.value = 1.5;
+    input.trigger('input');
+    formWrapper.find('input[type=submit]').trigger('click');
+
+    const error = formWrapper.find("#error-messages");
+
+    expect(error.exists()).toBeFalsy();
+  });
+
 });
